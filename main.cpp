ba2c390720a01a423a4d60acbd3572241e47e300
bgselly <benbigaselma@gmail.com>
2024-10-20 17:33:07 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+void tester_parametres(int* tableau, int taille) {
+    cout << taille << " paramètres executes." << endl;
+}
+
+int main() {
+    int nb_params = 10000;
+    int* tableau;
+
+    while (1) {
+
+        tableau = (int*) malloc(nb_params * sizeof(int));
+
+        if (tableau == NULL) {
+            cout << "Erreur : Impossible d'allouer la mémoire pour " << nb_params << " paramètres." << endl;
+            break;
+        }
+
+        tester_parametres(tableau, nb_params);
+        free(tableau);
+        nb_params += 100000;
+    }
+
+    return 0;
+}
